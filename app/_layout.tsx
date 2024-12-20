ff2cba4134a7a9bc68b4788f6d78e448556d8b1d
dodosaurus <kovac.jozef25@gmail.com>
2024-12-20 10:59:05 +0100

    implementing clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
+import { Colors } from "@/constants/Colors";
+import { tokenCache } from "@/utils/cache";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env");
 }
+
+const IntialLayout = () => {
+  return (
+    <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: Colors.background } }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+};
+
+const RootLayoutNav = () => {
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+        <IntialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
+};
+
+export default RootLayoutNav;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,129 @@
-import { Text, View } from "react-native";
+import { useOAuth } from "@clerk/clerk-expo";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Colors } from "@/constants/Colors";
 
 export default function Index() {
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_apple" });
+  const { startOAuthFlow: googleOAuth } = useOAuth({ strategy: "oauth_google" });
+  const { top } = useSafeAreaInsets();
+
+  const handleAppleOAuth = async () => {
+    try {
+      const { createdSessionId, setActive } = await startOAuthFlow();
+      console.log("appleOAuth ~ createdSessionId: " + createdSessionId);
+
+      if (createdSessionId) {
+        setActive!({
+          session: createdSessionId,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleGoogleOAuth = async () => {
+    try {
+      const { createdSessionId, setActive } = await googleOAuth();
+      console.log("googleOAuth ~ createdSessionId: " + createdSessionId);
+
+      if (createdSessionId) {
+        setActive!({
+          session: createdSessionId,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const openLink = async () => {
+    await WebBrowser.openBrowserAsync("http://www.jkovac.eu");
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello world.</Text>
+    <View style={[styles.container, { paddingTop: top }]}>
+      <Image source={require("../assets/images/todoist-logo.png")} style={styles.loginImage} />
+      <Image source={require("../assets/images/login.png")} style={styles.bannerImage} />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={handleAppleOAuth}>
+          <Ionicons name="logo-apple" size={24} color="black" />
+          <Text style={styles.buttonText}>Continue with Apple</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={handleGoogleOAuth}>
+          <Ionicons name="logo-google" size={24} color="black" />
+          <Text style={styles.buttonText}>Continue with Google</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Ionicons name="mail" size={24} color="black" />
+          <Text style={styles.buttonText}>Continue with Email</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.description}>
+          By continuing you agree to Todoist's{" "}
+          <Text style={styles.link} onPress={openLink}>
+            Terms of Service
+          </Text>{" "}
+          and{" "}
+          <Text style={styles.link} onPress={openLink}>
+            Privacy Policy
+          </Text>
+          .
+        </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 40,
+    marginTop: 20,
+  },
+  loginImage: {
+    height: 40,
+    resizeMode: "contain",
+    alignSelf: "center",
+  },
+  bannerImage: {
+    height: 280,
+    resizeMode: "contain",
+    alignSelf: "center",
+  },
+  buttonContainer: {
+    gap: 20,
+    marginHorizontal: 40,
+  },
+  button: {
+    backgroundColor: "#fff",
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+    borderRadius: 6,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.lightBorder,
+  },
+  buttonText: {
+    color: "#000",
+    fontWeight: 500,
+    fontSize: 20,
+  },
+  description: {
+    fontSize: 12,
+    textAlign: "center",
+    color: Colors.lightText,
+  },
+  link: {
+    color: Colors.lightText,
+    fontSize: 12,
+    textAlign: "center",
+    textDecorationLine: "underline",
+  },
+});
